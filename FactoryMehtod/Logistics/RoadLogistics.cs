542bd1ac533f9c173d502f98cba1da941f48fbe5
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-04-02 15:45:41 +0500

    INFRA : Initialize Factory Method

---
+++ FactoryMehtod/Logistics/RoadLogistics.cs
@@ -0,0 +1,18 @@
+﻿using FactoryMehtod.Services;
+
+namespace FactoryMehtod.Logistics
+{
+    public class RoadLogistics : ILogistics
+    {
+        private ITransport transport;
+        public ITransport CreateTransport()
+        {
+            return new Truck();
+        }
+
+        public void PlanDeleviry()
+        {
+            this.transport = CreateTransport();
+        }
+    }
+}
---
+++ FactoryMehtod/Transport/Ship.cs
@@ -0,0 +1,10 @@
+﻿namespace FactoryMehtod.Services
+{
+    public class Ship : ITransport
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Deliver by Sea in a container");
+        }
+    }
+}
